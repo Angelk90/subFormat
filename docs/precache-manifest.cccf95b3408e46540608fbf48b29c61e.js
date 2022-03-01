9a8648d0fc9c32742a436d64c64f2feeb1f8d866
Angelk90 <angelo.k90@hotmail.it>
2022-03-01 19:27:04 +0100

    Test diff

---
+++ docs/precache-manifest.cccf95b3408e46540608fbf48b29c61e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ce13b70d34a6a2a80da0cf5a9c4823",
+    "url": "/subFormat/index.html"
+  },
+  {
+    "revision": "f8420dba15873a63c996",
+    "url": "/subFormat/static/css/2.269e32a3.chunk.css"
+  },
+  {
+    "revision": "177df6872c110f9b335f",
+    "url": "/subFormat/static/css/main.e9d8e6c5.chunk.css"
+  },
+  {
+    "revision": "f8420dba15873a63c996",
+    "url": "/subFormat/static/js/2.ee17d02e.chunk.js"
+  },
+  {
+    "revision": "23b7dfcd50192b2ea8c66147ab4a583d",
+    "url": "/subFormat/static/js/2.ee17d02e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "177df6872c110f9b335f",
+    "url": "/subFormat/static/js/main.5e22caef.chunk.js"
+  },
+  {
+    "revision": "bd0e80dd719111aade10",
+    "url": "/subFormat/static/js/runtime-main.398abadd.js"
+  }
+]);
