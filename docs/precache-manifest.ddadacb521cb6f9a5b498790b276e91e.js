010a00c98f4643331850b7760e12820b0ddd78e2
Angelk90 <angelo.k90@hotmail.it>
2022-03-02 12:44:35 +0100

    Fix docs

---
+++ docs/precache-manifest.ddadacb521cb6f9a5b498790b276e91e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74378687edd8fc323b476e1eb7badb5f",
+    "url": "/subFormat/index.html"
+  },
+  {
+    "revision": "ef32216ad9839c2eaa5e",
+    "url": "/subFormat/static/css/2.b83cdfab.chunk.css"
+  },
+  {
+    "revision": "a8725e7673d408c5ea84",
+    "url": "/subFormat/static/css/main.6ec884c2.chunk.css"
+  },
+  {
+    "revision": "ef32216ad9839c2eaa5e",
+    "url": "/subFormat/static/js/2.27554451.chunk.js"
+  },
+  {
+    "revision": "21e2ef7debdab8734365c476c48ceb2d",
+    "url": "/subFormat/static/js/2.27554451.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a8725e7673d408c5ea84",
+    "url": "/subFormat/static/js/main.a58ce5f7.chunk.js"
+  },
+  {
+    "revision": "e9955860464255b12871",
+    "url": "/subFormat/static/js/runtime-main.95ce3791.js"
+  }
+]);
